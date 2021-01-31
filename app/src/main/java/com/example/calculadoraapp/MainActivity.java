8dc2aa8ada484e27ac885361e81f051c3423bb8a
Andres12309 <andrius12309@gmail.com>
2021-01-31 16:28:43 -0500

    Interfaz 4

--- app/src/main/java/com/example/calculadoraapp/MainActivity.java
+++ app/src/main/java/com/example/calculadoraapp/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.calculadoraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    
+    boolean decimal=false;
+    boolean parentesisOpen=false;
+    boolean parentesisClose=false;
+    boolean restar=false;
+    boolean suma=false;
+    boolean division=false;
+    boolean multiplicacion=false;
+    boolean potencia=false;
+    boolean porcentaje=false;
+    boolean sen=false;
+    boolean raiz=false;
+    boolean log=false;
+
+    Double[] numeros = new Double[10];
+    Double result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
